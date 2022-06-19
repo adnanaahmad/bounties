9651c7e728c439c06033655a3ab7bdd046936453
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2022-06-19 23:42:56 +0500

    react dndn added

--- src/components/bounty.js
+++ src/components/bounty.js
@@ -3,7 +3,7 @@ import { Box, Card, Typography } from "@mui/material";
 function BountyBoard() {
     return (
         <Box component={Card} sx={{maxWidth: 1000, marginX: 'auto', marginTop: 10, paddingX: 2, paddingY: 4}}>
-            <Typography>Bounties</Typography>
+            <Typography textAlign={'center'} variant='h4' fontWeight={600}>Bounties</Typography>
         </Box>
     );
   }
